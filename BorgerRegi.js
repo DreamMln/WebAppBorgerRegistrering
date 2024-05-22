9e2acf6de90ac4b026f7fe5f638f05bb4a25f8ed
DreamMln <maix2374@edu.zealand.dk>
2024-05-22 23:08:28 +0200

    ny

--- BorgerRegi.js
+++ BorgerRegi.js
@@ -6,23 +6,26 @@ const BorgerRegi = Vue.createApp({
             borgerTlfArray: [], //tomt array til at indeholde data omkring borgere
 			outputTlfData: "",
 			registrArray: [], //tomt array til registreringerne
-
+			pauseArray: [], //tomt array til pauser
 			borgerRegistreringer:
 			{
-			     ind: "",
-				 ud: "",
-					borgerPauser:
-						{
-							pauseStart: "",
-							pauseSlut: ""
-						}
+			     ind: null,
+				 ud: null,
+			},
+			//info fra backend, spørge serveren om
+			state: "not working",
+			borgerPauser:
+			{
+				pauseStart: null,
+				pauseSlut: null,
+			},
+			//info fra backend, spørge serveren om
+			statePause: "pause not started",
 
-				},
 			tlf: "",
 			id: 0,
 			//borgerID: 0,
 			addMessage: "",
-			objectDate: "",
 		}
     },
 	created(){ // life cycle method Called when browser reloads page
@@ -33,6 +36,7 @@ const BorgerRegi = Vue.createApp({
 
 		this.getRouteByBorgerTlf(tlf)
 		this.getAllRegiForEnBorger(this.id)
+		this.getAllPauserBorger(this.id)
 
 	},
 	methods: {
@@ -56,9 +60,7 @@ const BorgerRegi = Vue.createApp({
 		//based on their ID.
 		async getAllRegiForEnBorger(id){
 			try{
-			// Converts the date, to ISO format
-			let objectDate = new Date()
-			objectDate.toDateString()
+
 			//this.borgerID = id;
 			//hent regi data
 			const response = await axios.get(baseUrl + "/" + id + "/BorgerRegistreringer")
@@ -68,58 +70,110 @@ const BorgerRegi = Vue.createApp({
             alert("Fejl! Der er noget galt i hent regi" + response.status)
 		}
 		},
-		async sendRegi(id){
-			try{
-			//udfylde med dato og sende til serveren,
-			//( ikke modtage regiobj. kun sende/post)
-			//spørger, hvad tilstand er brugeren i - er han på arb, eller ej
-			//pauser - flere tilstande, er vi på pause eller ej?
-			//logget ind to mulig, pause eller gå hjem
-			//1. er jeg på arbejde og er jeg på pause
-			//to metoder
+		//1. sepererer ind og ud knapper V
+		//2. hvis borger logget ind
+		// er der to muligheder, slutpause eller ud
+		//=== strict ligheds-operator, compare to værdier, uden en konvertering af typen
+		async sendRegi(id, buttenClicked){
+			const dato = new Date().toDateString();
+			const tid = new Date().toLocaleTimeString();
 
-			// Converts the date, to ISO format
-			let objectDate = new Date();
-			//let day = objectDate.getDate();
-			//let month = objectDate.getMonth();
-			//let year = objectDate.getFullYear();
-			//let hours = objectDate.getHours();
-			//let minutes = objectDate.getMinutes();
-			//let seconds = objectDate.getSeconds();
-
-			this.borgerRegistreringer.ind = objectDate
-			this.borgerRegistreringer.ud = objectDate
-
-			//pause
-			this.borgerPauser.startPause = objectDate
-			this.borgerPauser.slutPause = objectDate
-
-			//this.borgerRegistreringer.ind = objectDate
-
-
-			//this.borgerRegistreringer.ind =
-			//date.toDateString();
-			//this.borgerRegistreringer.ud =
-			//date.toDateString();
+			if(this.state === 'not working'){
+				  if(buttenClicked === 'ind'){
+					this.borgerRegistreringer.ind = " Dato: " + dato + " - " + " Tid: " + tid;
+					this.borgerRegistreringer.ud = null; // Reset 'ud' if only 'ind' is set
+					//du er nu indregistreret og din status er working
+					this.state = "working";
+					//send et borgerregi obj. med borger id her sender den det/post
+					await axios.post(baseUrl + "/" + id + "/BorgerRegistreringer", this.borgerRegistreringer)
+					this.getAllRegiForEnBorger(id)
+					console.log(this.registrArray)
+		    	    }
+				    //sætte pause ind
+				    else if(buttenClicked === 'ud' || buttenClicked === 'pauseSlut'){
+					alert("Du kan ikke udregistrere/slutte en pause, før du er indregistreret!")
+				}
+			}
+			else if(this.state === 'working'){
+				if(buttenClicked === 'ud'){
+					this.borgerRegistreringer.ind = null; // Reset 'ind' if only 'ud' is set
+					this.borgerRegistreringer.ud = " Dato: " + dato + " - " + " Tid: " + tid;
+					//trykket ud - state ændres til ikke på arbejde
+					this.state = "not working";
+					//borgerens id, send et borgerregi obj. her sender den det/post
+			        await axios.post(baseUrl + "/" + id + "/BorgerRegistreringer", this.borgerRegistreringer)
+			        //after, refresh the data
+					//returns the updated registration array
+					this.getAllRegiForEnBorger(id)
+			        console.log(this.registrArray)
+		    }
+				//sætte pause ind
+				else if(buttenClicked === 'ind'){
+					alert("Du er allerede indregistreret!")
+				}
+			}
+			// 	//borger skal ikke kunne logge sig ud, før man er logget ind
+			// 	if(!this.borgerRegistreringer.ind){
+			// 		alert("Du skal være indregistreret før du kan udregistrere dig!");
+			// 		return
+			// }
+			//hvis du er indregistreret, så er der to muligheder,
+			//holde pause eller ud registrere sig
 		    console.log("ind: " + this.borgerRegistreringer.ind);
 			console.log("ud: " + this.borgerRegistreringer.ud);
-			//borgerens id, send et borgerregi obj. her sender den det/post
-			const response = await axios.post(baseUrl + "/" + id + "/BorgerRegistreringer", this.borgerRegistreringer)
-			//after, refresh the data
-			this.getAllRegiForEnBorger(id)
-			console.log(this.registrArray)
-			//https://www.freecodecamp.org/news/how-to-format-a-date-with-javascript-date-formatting-in-js/
-			}
-			catch(ex){
-				{alert("Fejl i opret registreringer! " + ex.message)}
-			}
 		},
-		async sendPause(){
-			let objectDate = new Date();
-
-			//pause
-			this.borgerPauser.startPause = objectDate
-			this.borgerPauser.slutPause = objectDate
+		/////////////BORGER PAUSER/////////
+		async getAllPauserBorger(id){
+			//this.borgerID = id;
+			try{
+			//hent pause data
+			const response = await axios.get(baseUrl + "/" + id + "/BorgerPauser")
+			this.pauseArray = await response.data
+			console.log(response.data)
+		}catch(ex){
+            alert("Fejl! Der er noget galt - hent pauser" + response.status)
+		}
+		},
+		async sendPause(id, buttonClicked){
+			const dato = new Date().toDateString();
+			const tid = new Date().toLocaleTimeString();
+			//=== operator is a strict equality operator in JavaScript. It compares two values for equality without performing any type conversion.
+			//are of the same type and have the same value, === returns true.
+			if (this.statePause === 'pause not started') {
+				if (buttonClicked === 'pauseStart') {
+					this.borgerPauser.pauseStart = " Dato: " + dato + " - " + " Tid: " + tid;
+					this.borgerPauser.pauseSlut = null; // Reset 'PauseSlut' if only 'PauseStart' is set
+					//pausen er nu startet
+					this.statePause = "pause started"
+					//send data pauser regi til serveren
+					await axios.post(baseUrl + "/" + id + "/BorgerPauser", this.borgerPauser)
+					//refresh
+					this.getAllPauserBorger(id)
+					console.log(this.pauseArray)
+				}
+				else if (buttonClicked === 'pauseSlut') {
+					alert("Du skal have startet en pause, før du kan afslutte en pause!");
+				}
+			}
+			else if (this.statePause === 'pause started') {
+				if(buttonClicked === 'pauseSlut'){
+					this.borgerPauser.pauseStart = null;// Reset 'pauseStart' if only 'PauseSlut' is set
+					this.borgerPauser.pauseSlut = " Dato: " + dato + " - " + " Tid: " + tid;
+					//state: pausen er nu afsluttet
+					this.statePause = "pause not started"
+					//send data pauser regi til serveren
+					await axios.post(baseUrl + "/" + id + "/BorgerPauser", this.borgerPauser)
+					//refresh the data after
+					this.getAllPauserBorger(id)
+					console.log(this.pauseArray)
+				}
+				else if (buttonClicked === 'pauseStart') {
+					alert("Du har allerede startet din pause!");
+				}
+		    }
+			console.log("Start: " + this.borgerPauser.pauseStart);
+			console.log("Slut: " + this.borgerPauser.pauseSlut);
+			console.log(this.pauseArray)
 		},
 	}
 })
